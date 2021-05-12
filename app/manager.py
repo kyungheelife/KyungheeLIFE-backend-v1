a90ea4f639f929f97a8b42fe178eb584e7b0f924
Euiseo Cha <zeroday0619_dev@outlook.com>
2021-05-12 10:50:27 +0900

    feat: ping pong 구현

--- app/manager.py
+++ app/manager.py
@@ -4,15 +4,25 @@ from starlette.websockets import WebSocket
 
 class ConnectionManager:
     def __init__(self):
-        self.connections: List[WebSocket] = []
+        self.active_connections: List[WebSocket] = []
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
-        self.connections.append(websocket)
+        self.active_connections.append(websocket)
 
-    async def broadcast(self, data: dict):
-        for connection in self.connections:
-            await connection.send_json(data)
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: dict):
+        for connection in self.active_connections:
+            await connection.send_json(message)
+
+    @staticmethod
+    async def send_personal_message(websocket: WebSocket, message: dict,):
+        await websocket.send_json(message)
+
+
+
 
 
 manager = ConnectionManager()
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import asyncio
-from starlette.websockets import WebSocket
+from starlette.websockets import WebSocket, WebSocketDisconnect
 from starlette.middleware.cors import CORSMiddleware
 
 from app.modules import School
@@ -14,7 +13,7 @@ from app.manager import manager
 
 core = KHSBackend(
     host="0.0.0.0",
-    port=8074,
+    port=3090,
     title="KyungheeLIFE BACKEND 3.0",
     description="KyungheeLIFE",
     debug=False
@@ -33,80 +32,102 @@ core.app.add_middleware(
 @core.app.websocket("/v1/ws/dashboard/covid19/total")
 async def covid_total(websocket: WebSocket):
     await manager.connect(websocket)
-    c = CovidStats()
-    while 1:
-        await asyncio.sleep(0.1)
-        await websocket.receive_text()
-        res = await c.ROKTotals()
-        await manager.broadcast(data=res)
+    try:
+        c = CovidStats()
+        while 1:
+            try:
+                resp = await websocket.receive_json()
+                if resp.get("message") != "ping":
+                    break
+                res = await c.ROKTotals()
+                await manager.broadcast(message=res)
+            except Exception as e:
+                print("error:", e)
+                break
+    except WebSocketDisconnect as e:
+        manager.disconnect(websocket)
+        msg = ReturnErrorMSG(status=False, code=e.code, message=f"Disconnected!").__dict__()
+        await manager.broadcast(message=msg)
 
 
 @core.app.websocket("/v1/ws/dashboard/school/lunch")
 async def lunch(websocket: WebSocket):
     await manager.connect(websocket)
-    sc = School()
-    while 1:
-        await asyncio.sleep(0.1)
-        try:
-            await websocket.receive_text()
-            nx = await sc.fetch_meal()
-            res = {
-                "status": True,
-                "system": {
-                    "code": 200,
-                    "message": "OK"
-                },
-                "data": [*nx[0].split()]
-            }
-            await manager.broadcast(data=res)
-        except Exception:
-            res = ReturnErrorMSG(
-                status=False,
-                code=500,
-                message="[ERROR] NEIS API Request Failed."
-            ).__dict__()
-            await manager.broadcast(data=res)
+    try:
+        sc = School()
+        while 1:
+            try:
+                resp = await websocket.receive_json()
+                if resp.get("message") != "ping":
+                    break
+                nx = await sc.fetch_meal()
+                res = {
+                    "status": True,
+                    "system": {
+                        "code": 200,
+                        "message": "OK"
+                    },
+                    "data": [*nx[0].split()]
+                }
+                await manager.broadcast(message=res)
+            except Exception as e:
+                print("error:", e)
+                break
+    except WebSocketDisconnect as e:
+        manager.disconnect(websocket)
+        msg = ReturnErrorMSG(status=False, code=e.code, message=f"Disconnected!").__dict__()
+        await manager.broadcast(message=msg)
 
 
 @core.app.websocket("/v1/ws/dashboard/school/dinner")
 async def dinner(websocket: WebSocket):
     await manager.connect(websocket)
-    sc = School()
-    while 1:
-        await asyncio.sleep(0.1)
-        try:
-            await websocket.receive_text()
-            nx = await sc.fetch_meal()
-            res = {
-                "status": True,
-                "system": {
-                    "code": 200,
-                    "message": "OK"
-                },
-                "data": [*nx[1].split()]
-            }
-            await manager.broadcast(data=res)
-        except Exception:
-            res = ReturnErrorMSG(
-                status=False,
-                code=500,
-                message="[ERROR] NEIS API Request Failed."
-            ).__dict__()
-            await manager.broadcast(data=res)
+    try:
+        sc = School()
+        while 1:
+            try:
+                resp = await websocket.receive_json()
+                if resp.get("message") != "ping":
+                    break
+                nx = await sc.fetch_meal()
+                res = {
+                    "status": True,
+                    "system": {
+                        "code": 200,
+                        "message": "OK"
+                    },
+                    "data": [*nx[1].split()]
+                }
+                await manager.broadcast(message=res)
+            except Exception as e:
+                print("error:", e)
+                break
+    except WebSocketDisconnect as e:
+        manager.disconnect(websocket)
+        msg = ReturnErrorMSG(status=False, code=e.code, message=f"Disconnected!").__dict__()
+        await manager.broadcast(message=msg)
 
 
 @core.app.websocket('/v1/ws/dashboard/weather/openweathermap')
 async def openweathermap_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
-    source = Weather()
-    while 1:
-        await asyncio.sleep(0.1)
-        try:
-            res = await source.fetch()
-            await manager.broadcast(data=res)
-        except Exception as e:
-            print("error: ", e)
-            break
+    try:
+        source = Weather()
+        while 1:
+            try:
+                resp = await websocket.receive_json()
+                if resp.get("message") != "ping":
+                    break
+                res = await source.fetch()
+                await manager.broadcast(message=res)
+            except Exception as e:
+                print("error: ", e)
+                break
+    except WebSocketDisconnect as e:
+        manager.disconnect(websocket)
+        msg = ReturnErrorMSG(status=False, code=e.code, message=f"Disconnected!").__dict__()
+        await manager.broadcast(message=msg)
+
 
 if __name__ == '__main__':
     core.start()
