bcc6f900bfe7010b178b162093ca9c77c855d4a4
Euiseo Cha <zeroday0619_dev@outlook.com>
2021-05-12 14:21:19 +0900

    feat: update

--- apis/v1/ws/dashboard/__init__.py
+++ apis/v1/ws/dashboard/__init__.py
@@ -4,6 +4,6 @@ from apis.v1.ws.dashboard.school import school_route
 from apis.v1.ws.dashboard.weather import weather_route
 
 dash_root_router = APIRouter()
-dash_root_router.include_router(covid_route, prefix="/covid")
+dash_root_router.include_router(covid_route, prefix="/covid19")
 dash_root_router.include_router(school_route, prefix="/school")
 dash_root_router.include_router(weather_route, prefix="/weather")
