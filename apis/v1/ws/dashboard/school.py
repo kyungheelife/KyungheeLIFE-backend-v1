63d6b34bf340e000bd7d7f4e581443b8d780ceda
Euiseo Cha <zeroday0619_dev@outlook.com>
2021-05-14 11:53:49 +0900

    feat: Contextmanager 구현

--- apis/v1/ws/dashboard/school.py
+++ apis/v1/ws/dashboard/school.py
@@ -5,66 +5,65 @@ from starlette.websockets import WebSocketDisconnect
 
 from app.modules import School
 from app.modules import ReturnErrorMSG
-from app.manager import manager
+from app.manager import ConnectionManager
 
 school_route = APIRouter()
 
 
 @school_route.websocket_route("/lunch")
 async def lunch(websocket: WebSocket):
-    await manager.connect(websocket)
-    try:
-        sc = School()
-        while 1:
-            try:
-                resp = await websocket.receive_json()
-                if resp.get("message") != "ping":
+
+    sc = School()
+    async with ConnectionManager(websocket=websocket) as ex:
+        try:
+            while 1:
+                try:
+                    resp = await ex.ping()
+                    if resp.get("message") != "ping":
+                        break
+                    nx = await sc.fetchLunch()
+                    data = nx.strip()
+                    res = {
+                        "status": True,
+                        "system": {
+                            "code": 200,
+                            "message": "OK"
+                        },
+                        "data": [*data]
+                    }
+                    await ex.broadcast(message=res)
+                except Exception as e:
+                    print("error:", e)
                     break
-                nx = await sc.fetch_meal()
-                data = nx[0].split()
-                res = {
-                    "status": True,
-                    "system": {
-                        "code": 200,
-                        "message": "OK"
-                    },
-                    "data": [*data]
-                }
-                await manager.broadcast(message=res)
-            except Exception as e:
-                print("error:", e)
-                break
-    except WebSocketDisconnect as e:
-        msg = ReturnErrorMSG(status=False, code=e.code, message=f"Disconnected!").__dict__()
-        await manager.broadcast(message=msg)
-        manager.disconnect(websocket)
+        except WebSocketDisconnect as e:
+            msg = ReturnErrorMSG(status=False, code=e.code, message=f"Disconnected!").__dict__()
+            await ex.broadcast(message=msg)
 
 
 @school_route.websocket_route("/dinner")
 async def dinner(websocket: WebSocket):
-    await manager.connect(websocket)
-    try:
-        sc = School()
-        while 1:
-            try:
-                resp = await websocket.receive_json()
-                if resp.get("message") != "ping":
+    sc = School()
+    async with ConnectionManager(websocket=websocket) as ex:
+        try:
+            while 1:
+                try:
+                    resp = await ex.ping()
+                    if resp.get("message") != "ping":
+                        break
+                    nx = await sc.fetchDinner()
+                    data = nx.strip()
+                    res = {
+                        "status": True,
+                        "system": {
+                            "code": 200,
+                            "message": "OK"
+                        },
+                        "data": [*data]
+                    }
+                    await ex.broadcast(message=res)
+                except Exception as e:
+                    print("error:", e)
                     break
-                nx = await sc.fetch_meal()
-                data = nx[1].split()
-                res = {
-                    "status": True,
-                    "system": {
-                        "code": 200,
-                        "message": "OK"
-                    },
-                    "data": [*data]
-                }
-                await manager.broadcast(message=res)
-            except Exception as e:
-                print("error:", e)
-                break
-    except WebSocketDisconnect as e:
-        msg = ReturnErrorMSG(status=False, code=e.code, message=f"Disconnected!").__dict__()
-        await manager.broadcast(message=msg)
-        manager.disconnect(websocket)
+        except WebSocketDisconnect as e:
+            msg = ReturnErrorMSG(status=False, code=e.code, message=f"Disconnected!").__dict__()
+            await ex.broadcast(message=msg)
--- apis/v1/ws/dashboard/weather.py
+++ apis/v1/ws/dashboard/weather.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from starlette.websockets import WebSocket
 from starlette.websockets import WebSocketDisconnect
 
-from app.manager import manager
+from app.manager import ConnectionManager
 from app.modules import Weather
 from app.modules import ReturnErrorMSG
 
@@ -13,20 +13,22 @@ weather_route = APIRouter()
 
 @weather_route.websocket_route('/openweathermap')
 async def openweathermap_endpoint(websocket: WebSocket):
-    await manager.connect(websocket)
     try:
         source = Weather()
-        while 1:
+        async with ConnectionManager(websocket=websocket) as ex:
             try:
-                resp = await websocket.receive_json()
-                if resp.get("message") != "ping":
-                    break
-                res = await source.fetch()
-                await manager.broadcast(message=res)
-            except Exception as e:
-                print("error: ", e)
-                break
-    except WebSocketDisconnect as e:
-        msg = ReturnErrorMSG(status=False, code=e.code, message=f"Disconnected!").__dict__()
-        await manager.broadcast(message=msg)
-        manager.disconnect(websocket)
+                while 1:
+                    try:
+                        resp = await websocket.receive_json()
+                        if resp.get("message") != "ping":
+                            break
+                        res = await source.fetch()
+                        await ex.broadcast(message=res)
+                    except Exception as e:
+                        print("error: ", e)
+                        break
+            except WebSocketDisconnect as e:
+                msg = ReturnErrorMSG(status=False, code=e.code, message=f"Disconnected!").__dict__()
+                await ex.broadcast(message=msg)
+    except Exception as a:
+        print("error:", a)
